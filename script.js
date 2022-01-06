b6883e2ae9f39770be42d075cf46bb214bd0a1f0
David Kim <cjoak1028@gmail.com>
2022-01-06 11:50:12 -0500

    added game reset functionality

--- script.js
+++ script.js
@@ -13,15 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting conditions
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -59,7 +72,7 @@ btnHold.addEventListener('click', function () {
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             // Finish the game
             diceEl.classList.add('hidden');
             playing = false;
@@ -71,3 +84,26 @@ btnHold.addEventListener('click', function () {
         }
     }
 })
+
+// My Horrible Solution
+// btnNew.addEventListener('click', function () {
+//     if (!playing) {
+//         document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+//     }
+
+//     scores[0] = 0;
+//     scores[1] = 0;
+//     currentScore = 0;
+//     activePlayer = 0;
+
+//     document.getElementById('current--0').textContent = 0;
+//     document.getElementById('current--1').textContent = 0;
+//     document.getElementById('score--0').textContent = 0;
+//     document.getElementById('score--1').textContent = 0;
+
+//     player0El.classList.add('player--active');
+//     player1El.classList.remove('player--active');
+// })
+
+// Tutorial Solution
+btnNew.addEventListener('click', init);
